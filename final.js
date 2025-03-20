eba1191ad51087a661439179099aba5c10f1d516
Avinash <ak3982621@gmail.com>
2025-03-20 11:19:35 +0530

    2nd commit

--- final.js
+++ final.js
@@ -1,3 +1,6 @@
 // today final github
 const randomNumber = Math.floor(Math.random() * 100)
 console.log("Random Number:", randomNumber);
+
+const randomString = "CoderArmy";
+console.log("Random String:", randomString);
